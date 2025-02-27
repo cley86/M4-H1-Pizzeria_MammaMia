670c5fc2dfbd7e42d0792f91766a7c763ea732bf
cley86 <cley.arevalo@gmail.com>
2025-02-27 15:57:12 -0600

    Actualización después del deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/<M4-H1-Pizzeria_MammaMia>/", // Reemplaza <repository-name> con el nombre de tu repositorio
   plugins: [react()],
-})
+});
